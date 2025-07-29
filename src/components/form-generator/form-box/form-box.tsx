2d791684eedad80e163fcfc6532103491c3ad3fd
Shaan <shaanduss@gmail.com>
2025-07-29 16:58:45 +0800

    Fix: FormBox styling

--- src/components/form-generator/form-box/form-box.tsx
+++ src/components/form-generator/form-box/form-box.tsx
@@ -3,22 +3,32 @@ import { FormSection } from "./form-section"
 import { Separator } from "@/components/ui/separator"
 import React from "react"
 
-export const FormBox: React.FC<FormBoxProps> = (props) => {
+import { cn } from "@/lib/utils"
+
+export const FormBox: React.FC<FormBoxProps> = ({
+  boxName,
+  sections,
+  boxNameString,
+  nameStyling = "text-foreground"
+}) => {
   return(
-    <div className="w-full">
-      {(typeof props.boxName == "string") ?
-      <p className="font-extrabold text-xl lg:text-[24px] w-full text-left border-b text-foreground py-3">{props.boxName}</p>
-      :
-      <>{props.boxName}</>
-    }
+    <div className="w-full" key={(typeof boxName == "string") ? boxName : boxNameString}>
+      {(typeof boxName == "string") ?
+        <p className={cn("font-extrabold text-xl lg:text-[24px] w-full text-left border-b py-3", nameStyling)}>
+          {boxName}
+        </p>
+        :
+        <>{boxName}</>
+      }
+
       <div className="w-full">
-          {props.sections.map((section, idx) => (
+          {sections.map((section, idx) => (
             <React.Fragment key={(typeof section.sectionName == "string") ? section.sectionName : section.sectionNameString}>
               <FormSection sectionName={section.sectionName}
                 sectionDescription={section.sectionDescription}
                 blocks={section.blocks}
               />
-              {idx !== props.sections.length - 1 && <Separator />}
+              {idx !== sections.length - 1 && <Separator />}
             </React.Fragment>
           ))}
       </div>
--- src/interfaces/FormBoxInterfaces.tsx
+++ src/interfaces/FormBoxInterfaces.tsx
@@ -1,6 +1,6 @@
 export interface FormBlockProps {
   label: string | React.ReactNode;
-  type: "address" | "select" | "checkbox" | "symbolInputLeft" | "symbolInputRight" | "input" | "input-select" | "radio" | "button-input" | "input-label" | "text-area";
+  type: "address" | "select" | "checkbox" | "symbolInputLeft" | "symbolInputRight" | "input" | "input-select" | "radio" | "button-input" | "input-label" | "text-area" | "symbolLeft-input-select";
   layout?: "full-row" | "default" | "row-self";
   selectOptions?: string[];
   selectOptionsLabels?: string[];
@@ -32,4 +32,5 @@ export interface FormBoxProps {
   boxName: string | React.ReactNode;
   sections: FormSectionProps[];
   boxNameString?: string;
+  nameStyling?: string;
 }
